11be058c1bd35d80be95e712b5938b4cf92aee58
nikitabuts <nvbuts@edu.hse.ru>
2022-12-23 17:43:29 +0300

    initial commit 1.0.8

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
 import torch
-from pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample,
-                                       save_as_images, display_in_terminal)
+from PIL import Image
+import numpy as np
+from pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample)
 
-import streamlit as st
+#import streamlit as st
 import random
+
+import logging as logger
 
 from typing import Tuple
 
 import nltk 
 nltk.download('popular')
+
+def convert_to_images(obj):
+    """ Convert an output tensor from BigGAN in a list of images.
+        Params:
+            obj: tensor or numpy array of shape (batch_size, channels, height, width)
+        Output:
+            list of Pillow Images of size (height, width)
+    """
+    try:
+        import PIL
+    except ImportError:
+        raise ImportError("Please install Pillow to use images: pip install Pillow")
+
+    if not isinstance(obj, np.ndarray):
+        obj = obj.detach().numpy()
+
+    obj = obj.transpose((0, 2, 3, 1))
+    obj = np.clip(((obj + 1) / 2.0) * 256, 0, 255)
+
+    img = []
+    for i, out in enumerate(obj):
+        out_array = np.asarray(np.uint8(out), dtype=np.uint8)
+        img.append(Image.fromarray(out_array))
+    return img
+
+
+def save_as_images(obj, file_name='output'):
+    """ Convert and save an output tensor from BigGAN in a list of saved images.
+        Params:
+            obj: tensor or numpy array of shape (batch_size, channels, height, width)
+            file_name: path and beggingin of filename to save.
+                Images will be saved as `file_name_{image_number}.png`
+    """
+    img = convert_to_images(obj)
+
+    for i, out in enumerate(img):
+        current_file_name = file_name + '_%d.png' % i
+        logger.info("Saving image to {}".format(current_file_name))
+        out.save(current_file_name, 'png')
 
 @st.cache
 def load_model(pretrained_name: str):
@@ -56,6 +98,6 @@ if name is not None and st.button('Generate'):
     if seed is not None and (seed <= 0 or seed > 10000):
         seed = None
     
-    output = model.predict(name=name, seed=seed)
+    output = model.predict(name="cup", seed=None)
     save_as_images(output, file_name="output")
-    st.image("output")
+    st.image("output_0.png")
